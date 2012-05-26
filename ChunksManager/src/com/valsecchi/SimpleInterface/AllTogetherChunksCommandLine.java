b3ad471f7c4cdb1ef690ef62792d045a867858e0
Davide_Valsecchi <davide.vals94@gmail.com>
2012-05-26 19:03:04 +0200

    creazione prime costanti per i comandi
    inizio creazione loop per la console, primi comandi inseriti.

--- ChunksManager/src/com/valsecchi/SimpleInterface/AllTogetherChunksCommandLine.java
+++ ChunksManager/src/com/valsecchi/SimpleInterface/AllTogetherChunksCommandLine.java
@@ -1,6 +1,11 @@
 package com.valsecchi.SimpleInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.valsecchi.ChunksManager.*;
+import static java.lang.System.out;
 
 /**
  * Classe che fornisce una semplicissima interfaccia a linea di comando per
@@ -12,12 +17,42 @@ import com.valsecchi.ChunksManager.*;
  */
 public class AllTogetherChunksCommandLine {
 
+	// vcostanti per il controllo dei comandi inseriti dall'utente
+	private static final String HELP = "help";
+	private static final String FIND = "find";
+
+	private static final String[] COMMANDS = { "help", "find" };
 	/**
-	 * 
-	 * @param args
+	 * Variabile che ferma il loop del programma
 	 */
-	public static void main(String[] args) {
+	private static boolean canContinue = true;
 
+	/**
+	 * Viene eseguito un loop continuo per gestire i comandi
+	 */
+	public static void main(String[] args) throws IOException {
+		out.println("Benvenuti nel programma All Together Chunks!\nDigitare "
+				+ "un comando e help per una lista dei comandi disponibili...");
+
+		// viene creato il lettore dell'input della console
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		// comando inserito
+		String command = "";
+		// inizio loop
+		while (canContinue) {
+			command = reader.readLine();
+			// si controlla il comando scelto
+			switch (command) {
+			case HELP: {
+				// si scrivono i comandi disponibili
+				for (String c : COMMANDS) {
+					out.println(c);
+				}
+				break;
+			}
+
+			}
+		}
 	}
-
 }
