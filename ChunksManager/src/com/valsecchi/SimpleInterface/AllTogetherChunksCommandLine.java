83f41450852d11bb29da9c63b576064be0947db0
Davide_Valsecchi <davide.vals94@gmail.com>
2012-05-26 18:49:46 +0200

    inizio lavoro sull'interfaccia grafica a linea di comando
    creazione classe AllTogetherChunksCommandLine per fornire
    all'applicazione una semplice interfaccia a linea di comando.

---
+++ ChunksManager/src/com/valsecchi/SimpleInterface/AllTogetherChunksCommandLine.java
@@ -0,0 +1,23 @@
+package com.valsecchi.SimpleInterface;
+
+import com.valsecchi.ChunksManager.*;
+
+/**
+ * Classe che fornisce una semplicissima interfaccia a linea di comando per
+ * ChunksManager.
+ * 
+ * @author Davide Valsecchi
+ * @version v.0.0.0
+ * @since v.0.0.0
+ */
+public class AllTogetherChunksCommandLine {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
