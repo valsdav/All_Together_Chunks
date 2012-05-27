093febfd7038d0cf2cbc9d99045691141ea453cc
Davide_Valsecchi <davide.vals94@gmail.com>
2012-05-27 12:36:28 +0200

    aggiunta lista dei comandi (ancora vuota)
    aggiunta lista dei comandi con: lista di comandi, lista delle
    istruzioni, e mappa in cui associare istruzioni e comandi

--- ChunksManager/src/com/valsecchi/SimpleInterface/AllTogetherChunksCommandLine.java
+++ ChunksManager/src/com/valsecchi/SimpleInterface/AllTogetherChunksCommandLine.java
@@ -3,6 +3,11 @@ package com.valsecchi.SimpleInterface;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import com.valsecchi.ChunksManager.*;
 import static java.lang.System.out;
@@ -17,20 +22,37 @@ import static java.lang.System.out;
  */
 public class AllTogetherChunksCommandLine {
 
-	// vcostanti per il controllo dei comandi inseriti dall'utente
+	// singoli comandi
 	private static final String HELP = "help";
 	private static final String FIND = "find";
-
-	private static final String[] COMMANDS = { "help", "find" };
+	/**
+	 * Array di stringhe che contiene i vari comandi che saranno poi inseriti in
+	 * {@link #COMMANDS_MAP}
+	 */
+	private static final String[] COMMANDS_LIST = { HELP, FIND };
+	/**
+	 * Array di stringe che contiene le istruzioni dei vari comandi che saranno
+	 * poi inseriti in {@link #COMMANDS_MAP}
+	 */
+	private static final String[] COMMANDS_INSTR = { "richiede aiuto",
+			"aggiunge un chunk" };
+	/**
+	 * Mappa che incapsula tutti i comandi disponibili con relativa
+	 * documentazione.
+	 */
+	private static Map<String, String> COMMANDS_MAP;
 	/**
 	 * Variabile che ferma il loop del programma
 	 */
 	private static boolean canContinue = true;
 
 	/**
-	 * Viene eseguito un loop continuo per gestire i comandi
+	 * Loop continuo del programma che interpreta ed esegue i comandi
 	 */
 	public static void main(String[] args) throws IOException {
+		//si caricano i comandi
+		setupInstructions();
+		
 		out.println("Benvenuti nel programma All Together Chunks!\nDigitare "
 				+ "un comando e help per una lista dei comandi disponibili...");
 
@@ -42,17 +64,38 @@ public class AllTogetherChunksCommandLine {
 		// inizio loop
 		while (canContinue) {
 			command = reader.readLine();
-			// si controlla il comando scelto
-			switch (command) {
-			case HELP: {
-				// si scrivono i comandi disponibili
-				for (String c : COMMANDS) {
-					out.println(c);
-				}
-				break;
+			// si divide il comando nelle sue parti
+			StringTokenizer tok = new StringTokenizer(command);
+			List<String> cmds = new ArrayList<>();
+			while (tok.hasMoreTokens()) {
+				cmds.add(tok.nextToken());
 			}
+			// ora abbiamo i comandi incapsulati
+			// si controlla il comando primo comando scelto
+			switch (cmds.get(0)) {
+			case HELP: {
+				// si controlla quale comando segue help
+				if (cmds.size() > 1) {
+
+				} else {
+					// allora bisogna scrivere tutti i comandi a schermo con
+					// relative istruzioni
+					for(int i = 0; i<= COMMANDS_MAP.size();i++){
+//						out.println(COMMANDS_MAP.keySet().,COMMANDS_MAP.values()[i]);
+					}
+				}
 
 			}
+			}
+		}
+	}
+	
+	/**
+	 * Il metodo inserisce in {@link #COMMANDS_MAP} tutti i comandi con relative istruzioni
+	 */
+	private static void setupInstructions(){
+		for(int i= 0; i<= COMMANDS_LIST.length;i++){
+			COMMANDS_MAP.put(COMMANDS_LIST[i], COMMANDS_INSTR[i]);
 		}
 	}
 }
